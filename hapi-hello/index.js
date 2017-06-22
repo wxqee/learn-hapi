2f6e7a924f7964e25cde78e6f259be1b50fcce45
Robin Wang <robin.wang@otms.com>
2017-06-22 22:33:02 +0800

    initial commit

---
+++ hapi-hello/index.js
@@ -0,0 +1,19 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({ port: 1337, host: '127.0.0.1' });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    return reply('Hello World\n');
+  }
+});
+
+server.start((err) => {
+  if(err) {
+    throw err;
+  }
+
+  console.log(`Server running at ${server.info.uri}`);  // [5]
+});
